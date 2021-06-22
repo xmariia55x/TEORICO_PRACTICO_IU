c36fe6d044689567bc3cea4bc68f165cd1fcab9f
AdriCorrales <adricorrales@uma.es>
2021-06-22 13:25:20 +0200

    objetos competicion

---
+++ javascript/jsCompeticion.js
@@ -0,0 +1,11 @@
+
+function Competicion(nombre, fecha, hora, tipoArco, categoria, rondas, numFlechas, limTiempo) {
+    this.nombre = nombre;
+    this.fecha = fecha;
+    this.hora = hora;
+    this.tipoArco = tipoArco;
+    this.categoria = categoria;
+    this.rondas = rondas;
+    this.numFlechas = numFlechas;
+    this.limTiempo = limTiempo;
+}
