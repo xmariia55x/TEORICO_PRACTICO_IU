9096410f17422358b2885fa97c9b1f06312d464b
YerayRuiz <yeray.r7@uma.es>
2021-06-24 17:34:19 +0200

    commit temporizador

--- javascript/jsTemporizador.js
+++ javascript/jsTemporizador.js
@@ -1,5 +1,5 @@
-var temp = 10000;
-var tempAux = 10000;
+var temp = 2000;
+var tempAux = 2000;
 visor = document.getElementById("reloj"); //localizar pantalla del reloj
 
 
@@ -18,11 +18,10 @@ window.onload = function () {
         var mensaje;
         var opcion = confirm("¿Está seguro que desea salir de la competición? Se borraran todos los datos introducidos hasta ahora.");
         if (opcion === true) {
-            mensaje = "Has clickado OK";
+            location.href = "infoCompeticion.html";
         } else {
-            mensaje = "Has clickado Cancelar";
+            return;
         }
-        alert(mensaje);
     });
 
     //variables de inicio:
@@ -116,7 +115,7 @@ function reiniciar() {
     }
     //en cualquier caso volvemos a los valores iniciales
     cro = 0; //tiempo transcurrido a cero
-    visor.innerHTML = "00 mm 10 ss 00 ms"; //visor a cero
+    visor.innerHTML = "00 mm 02 ss 00 ms"; //visor a cero
     document.cron.boton1.value = "Empezar"; //estado inicial primer botón
     document.cron.boton2.value = "Parar"; //estado inicial segundo botón
     document.cron.boton2.disabled = true;  //segundo botón desactivado
@@ -140,41 +139,37 @@ function showTab(n) {
 }
 
 function nextPrev() {
-    if (rondaActual > rondasTotales){  //Final -> volver a inicio -> mensaje de confirmación
-        
-    }
-
     var x = document.getElementsByClassName("tab");
     x[currentTab].style.display = "none";
-    var inputPtsActual = document.getElementsByClassName('columna'+rondaActual);
+    var inputPtsActual = document.getElementsByClassName('columna' + rondaActual);
 
     if (currentTab === 0) {        //CRONOMETRO
         currentTab = 1;
 
         //HABILITAR LOS INPUT QUE TOQUEN Y DESHABILITAR LOS DE LA RONDA ANTERIOR
         if (rondaActual > 1) {
-            var inputPtsAnterior = document.getElementsByClassName('columna'+(rondaActual-1));
-            for (var i = 0; i < inputPtsAnterior.length; i++){
-                inputPtsAnterior[i].setAttribute('disabled','');
+            var inputPtsAnterior = document.getElementsByClassName('columna' + (rondaActual - 1));
+            for (var i = 0; i < inputPtsAnterior.length; i++) {
+                inputPtsAnterior[i].setAttribute('disabled', '');
             }
         }
-        
-        for (var i = 0; i < inputPtsActual.length; i++){
+
+        for (var i = 0; i < inputPtsActual.length; i++) {
             inputPtsActual[i].disabled = false;
         }
 
-        if (rondaActual === rondasTotales){
+        if (rondaActual === rondasTotales) {
             //CAMBIAR BOTON SIGUIENTE RONDA POR FINALIZAR COMPETICIÓN
-            bSiguiente.value = 'Finalizar competición'
+            bSiguiente.value = 'Ver resultado'
         }
 
         bSiguiente.style.display = "inline";
 
-    } else {                       //PUNTUACIONES
+    } else if (currentTab === 1) {                       //PUNTUACIONES
         var valido = validarPuntos();
-        if (valido.length > 0){
+        if (valido.length > 0) {
             alert("Campos no válidos");
-            for (var i = 0; i < valido.length; i++){
+            for (var i = 0; i < valido.length; i++) {
                 inputPtsActual[valido[i]].focus();
             }
             return;
@@ -182,31 +177,103 @@ function nextPrev() {
             var opcion = confirm("Pasará a la siguiente ronda y no habrá vuelta atrás. ¿Está seguro?");
             if (opcion === true) {
                 calcularPuntos(rondaActual);
-                currentTab = 0;
-                rondaActual++;
-                reiniciar();
-                bSiguiente.style.display = "none";
-                lRonda.innerHTML = "Ronda "+rondaActual+" de 12";
+                if (rondaActual < rondasTotales) {
+                    currentTab = 0;
+                    rondaActual++;
+                    reiniciar();
+                    bSiguiente.style.display = "none";
+                    lRonda.innerHTML = "Ronda " + rondaActual + " de 12";
+                } else {
+                    currentTab = 2;
+
+                    var x = document.getElementsByClassName("tab");
+                    x[currentTab].style.display = "none";
+                    currentTab = 2;
+                    var tabla = document.getElementById('tablafinal');
+                    var columnaParticipantes = document.querySelectorAll('.participantes');
+                    var columnaTotal = document.querySelectorAll('.columna13');
+                    var lista = [];
+                    var primero = document.getElementById("primero");
+                    var segundo = document.getElementById("segundo");
+                    var tercero = document.getElementById("tercero");
+
+                    for (var i = 0; i < columnaTotal.length; i++) {
+                        var elem = new Object();
+                        elem.nombre = columnaParticipantes[i].textContent;
+                        elem.puntuacion = columnaTotal[i].value;
+
+                        lista.push(elem);
+                    }
+
+                    lista.sort(function (a, b) {
+                        if (a.puntuacion > b.puntuacion) {
+                            return -1;
+                        } else if (a.puntuacion < b.puntuacion) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    });
+
+                    primero.innerHTML = lista[0].nombre;
+                    segundo.innerHTML = lista[1].nombre;
+                    tercero.innerHTML = lista[2].nombre;
+
+                    var tr = document.createElement('tr');
+                    tabla.appendChild(tr);
+
+                    var th1 = document.createElement('th');
+                    th1.appendChild(document.createTextNode("Posición"));
+                    tr.appendChild(th1);
+                    var th2 = document.createElement('th');
+                    th2.appendChild(document.createTextNode("Nombre"));
+                    tr.appendChild(th2);
+                    var th3 = document.createElement('th');
+                    th3.appendChild(document.createTextNode("Puntuación"));
+                    tr.appendChild(th3);
+
+                    for (var i = 0; i < lista.length; i++) {
+                        //añadir fila a la tabla
+                        var tr = document.createElement('tr');
+                        tabla.appendChild(tr);
+
+                        var td1 = document.createElement('td');
+                        td1.appendChild(document.createTextNode(i + 1));
+                        tr.appendChild(td1);
+                        var td2 = document.createElement('td');
+                        td2.appendChild(document.createTextNode(lista[i].nombre));
+                        tr.appendChild(td2);
+                        var td3 = document.createElement('td');
+                        td3.appendChild(document.createTextNode(lista[i].puntuacion));
+                        tr.appendChild(td3);
+                    }
+
+                    bSiguiente.value = 'Finalizar competición';
+                }
             } else {
                 return;
             }
         }
+    } else {
+        //FINALIZA LA COMPETICIÓN
+        location.href = "../html/listadoCompeticionesMaster.html";
     }
+        
 
     showTab(currentTab);
 }
 
-function validarPuntos(){
-    var columna = document.querySelectorAll('.columna'+rondaActual);
-    var error = []; 
+function validarPuntos() {
+    var columna = document.querySelectorAll('.columna' + rondaActual);
+    var error = [];
     var contador = 0;
-    for(var i = 0; i < columna.length; i++){
+    for (var i = 0; i < columna.length; i++) {
         try {
-            if (Number(columna[0].value) < 0){
+            if (columna[i].value.length === 0 || Number(columna[i].value) < 0) {
                 error[contador] = i + 1;
                 contador++;
             }
-        } catch (error){
+        } catch (error) {
             error[contador] = i + 1;
             contador++;
         }
@@ -248,9 +315,10 @@ function addTable() {
                     td.appendChild(document.createTextNode("Participantes"));
                     td.style = 'width:max-content';
                 }
-                
+
             } else if (j === 0 && i !== 0) {
                 td.appendChild(document.createTextNode("Participante " + i));
+                td.classList.add('participantes');
                 td.style = 'width:max-content';
             } else {
                 var txtInput = document.createElement('input');
@@ -258,16 +326,16 @@ function addTable() {
                 txtInput.setAttribute('id', 'celda' + i + j);
                 txtInput.setAttribute('size', '3');
                 txtInput.setAttribute('maxlength', '3');
-                txtInput.setAttribute('disabled','');
+                txtInput.setAttribute('disabled', '');
                 txtInput.classList.add('txtPuntos');
-                if(j >= 1 && j < 13){
+                if (j >= 1 && j < 13) {
                     txtInput.classList.add('fila' + i);
                 }
                 if (j === 13) {
                     txtInput.setAttribute('readOnly', 'true');
                     txtInput.style = 'background-color: #C3FFF3';
 
-                } 
+                }
                 td.appendChild(txtInput);
 
             }
@@ -277,12 +345,12 @@ function addTable() {
     myTableDiv.appendChild(table);
 }
 
-function calcularPuntos(rondaActual){
-    var columnaTotal = document.querySelectorAll('.columna13'); 
-    for(var i = 1; i <= participantes; i++){
+function calcularPuntos(rondaActual) {
+    var columnaTotal = document.querySelectorAll('.columna13');
+    for (var i = 1; i <= participantes; i++) {
         var fila = document.querySelectorAll('.fila' + i);
         var suma = 0;
-        for(var j = 1; j <= rondaActual; j++){
+        for (var j = 1; j <= rondaActual; j++) {
             suma += Number(fila[j - 1].value);
         }
         columnaTotal[i - 1].value = suma;
